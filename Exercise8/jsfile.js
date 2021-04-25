5b4343591da40d1c382cbd2ed0a774164dc1f116
ThanasisKl <tklettas@gmail.com>
2021-04-25 15:30:38 +0300

    Exercise 8 DONE

--- Exercise8/jsfile.js
+++ Exercise8/jsfile.js
@@ -45,17 +45,70 @@ function changeHTML2(){   //HTML Change
     document.getElementById("input").innerHTML = "<input type='submit' onclick='change2Blue()' id='sumbit' name='submit' value='Dark Theme'>";
 }
 
-function usernameHandler(event){
-    var username = document.getElementById("username")
-    console.log(username);
-    //if(username.value)
-    let flag = true;
+// Exercise 8
+window.onload = function() {
+    document.getElementById("submit").addEventListener("click", emailHandler);
+    document.getElementById("submit").addEventListener("click", dateHandler);
+    document.getElementById("submit").addEventListener("click", passwordHandler);    
+}
 
-    if(flag){
-        username.setCustomValidity('ghghghghhg');
+function passwordHandler(){
+    let password1 = document.getElementById("password1");
+    let password2 = document.getElementById("password2");
+
+    if(password1.value === password2.value){
+        password2.setCustomValidity('');
     }else{
-        username.setCustomValidity('');
+        password2.setCustomValidity('Οι κωδικοί πρέπει να ταιριάζουν μεταξύ τους');
     }
 }
 
-window.addEventListener('load',usernameHandler)
+function dateHandler(){
+    let userDate = document.getElementById("date");
+    let currentDate = new Date();
+    let array = userDate.value.split("-");
+
+    let currentYear = currentDate.getFullYear();
+    let userYear = parseInt(array[0]);
+    if(currentYear - userYear > 110 || currentYear - userYear < 0){
+        userDate.setCustomValidity('Παρακαλώ βάλτε έγκυρες τιμές');
+    }else{
+        let flag = true;
+        if(currentYear - userYear > 18){
+            flag = true;
+        }else if(currentYear - userYear === 18){
+            let userMonth = parseInt(array[1]);
+            let currentMonth = currentDate.getMonth()+1;
+            if(userMonth > currentMonth){
+                flag = false;
+            }else if(userMonth === currentMonth){
+                let userDay = parseInt(array[2]);
+                let currentDay = currentDate.getDate();
+                if(userDay > currentDay){
+                    flag = false;
+                }else{
+                    flag = true;
+                }
+            }else{
+                flag = true;
+            }
+        }else{
+            flag = false;
+        }
+
+        if(flag){
+            userDate.setCustomValidity('');
+        }else{
+            userDate.setCustomValidity('Είσαι κάτω από 18 δεν μπορείς να δημιουργήσεις λογιαριασμό');
+        }
+    }
+}
+
+function emailHandler(){
+    let email = document.getElementById("mail");
+    if(email.value.trim() === "p3180079@aueb.gr"){
+        email.setCustomValidity('Αυτό το email δεν είναι δικό σου');
+    }else{
+        email.setCustomValidity('');
+    }
+}
